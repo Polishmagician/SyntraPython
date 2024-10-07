367efd6bf452dcac292b282eed295b867e146c4d
Jorden <fcjory@gmail.com>
2024-10-07 21:40:08 +0200

    test van pycharm naar github

---
+++ les_9.py
@@ -0,0 +1,7 @@
+from tabulate import tabulate
+
+
+woordenboek = {"table":"tafel","car":"auto","dog":"hond","charger":"lader","book":"boek","church":"kerk","chair":"stoel","sound":"geluid","apple":"appel","mouse":"muis"}
+kolom = ["Engels","Nederlands"]
+# print(tabulate(woordenboek.items(), headers=kolom))
+
